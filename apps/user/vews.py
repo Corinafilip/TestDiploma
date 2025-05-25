5a81e3efcfef793dbff6f8a267b94b2e408f7890
Corina <photobycorina@gmail.com>
2025-05-25 17:59:51 +0200

    jwt login logout using simplejwt

--- apps/user/vews.py
+++ apps/user/vews.py
@@ -5,6 +5,11 @@ from rest_framework.views import APIView
 from .models import User
 from .serializers import UserRegistrationSerializer, UserSerializer
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 
 class RegisterUserAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -18,3 +23,16 @@ class CurrentUserAPIView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
